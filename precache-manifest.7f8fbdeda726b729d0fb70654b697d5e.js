13ba257f3000a390b32db646fe9d9b340393cff5
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-06-20 18:24:35 +0530

    Updates

---
+++ precache-manifest.7f8fbdeda726b729d0fb70654b697d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bddfedb95af3cb693f399f822032a1",
+    "url": "/Quarantimes/index.html"
+  },
+  {
+    "revision": "e3f7805ce30a7b91d40f",
+    "url": "/Quarantimes/static/css/main.a0c2f721.chunk.css"
+  },
+  {
+    "revision": "d5326d8ece36b0b44563",
+    "url": "/Quarantimes/static/js/2.fd80c42a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Quarantimes/static/js/2.fd80c42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f7805ce30a7b91d40f",
+    "url": "/Quarantimes/static/js/main.35112abb.chunk.js"
+  },
+  {
+    "revision": "fbed52bef412a0e5f77e",
+    "url": "/Quarantimes/static/js/runtime-main.df19b77e.js"
+  }
+]);
